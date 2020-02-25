f0a29763b93070855932b23a52e62d3e86572759
gopal-mac <gopal@appnings.com>
2020-02-25 22:44:06 +0530

    init commit

---
+++ src/emails/account.js
@@ -0,0 +1,26 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'nandini.appnings@gmail.com',
+        subject: 'Thanks for joining us!',
+        text: `Welcometo the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'nandini.appnings@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
